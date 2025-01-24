b30081976f1966e32e0b77d234c49e16b42fc1f5
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-24 19:30:28 -0300

    added http server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World ! ");
+import * as http from "http";
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Server initialized in port ${port}`);
+});
