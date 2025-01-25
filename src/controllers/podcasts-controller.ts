27e4b7bfd6620d62b78726ab920a40a2bf9d20b3
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-24 23:14:38 -0300

    added services

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,11 +1,13 @@
-import { IncomingMessage, request, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from "http";
+
+import { serviceListEpisodes } from "../services/list-episodes-service";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
+  const content = await serviceListEpisodes();
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end({
-    name: "felipe",
-  });
+  res.end(JSON.stringify(content));
 };
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,18 @@
+export const serviceListEpisodes = async () => {
+  const data = [
+    {
+      podcastName: "flow",
+      episodeName: "CBUM - Flow #319",
+      videoId: "pQSuQmUfS30",
+      categories: ["saude", "esporte", "bodybuilder"],
+    },
+    {
+      podcastName: "flow",
+      episodeName: "RUBENS BARRICHELO- Flow #339",
+      videoId: "pQSuQmUfS30",
+      categories: ["esporte", "corrida"],
+    },
+  ];
+
+  return data;
+};
