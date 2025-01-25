c844a0810ee77803b528118dfc6d1eef11573268
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-25 13:57:17 -0300

    added service

---
+++ src/services/flter-episodes-service.ts
@@ -0,0 +1,5 @@
+import { repositoryPodcast } from "../repositories/podcast-repository";
+
+export const serviceFilterEpisodes = async (podcastName: string) => {
+  const data = await repositoryPodcast(podcastName);
+};
