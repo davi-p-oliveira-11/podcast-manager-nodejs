63f6725aaba28e72a857ed170fc2dd2300a61bb9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-26 20:25:46 -0300

    added more stayus code

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -10,7 +10,7 @@ export const getListEpisodes = async (
 ) => {
   const content = await serviceListEpisodes();
 
-  res.writeHead(200, { "Content-Type": "application/json" });
+  res.writeHead(StatusCode.OK, { "Content-Type": "application/json" });
   res.end(JSON.stringify(content));
 };
 
