1925310abf6749c4d2319a58c10769894493d332
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-26 20:54:49 -0300

    added app.ts

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import * as http from "http";
+
+import {
+  getFilterEpisodes,
+  getListEpisodes,
+} from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
+
+export const app = async (
+  request: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
+  // query string
+  // http://localhost:3333/api/episode?p=flow
+  const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
+
+  // listar podcasts
+  if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
+    await getListEpisodes(request, res);
+  }
+
+  if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
+    await getFilterEpisodes(request, res);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,6 @@
 import * as http from "http";
 
-import {
-  getFilterEpisodes,
-  getListEpisodes,
-} from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
-
-const server = http.createServer(
-  async (request: http.IncomingMessage, res: http.ServerResponse) => {
-    // query string
-    // http://localhost:3333/api/episode?p=flow
-    const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-
-    // listar podcasts
-    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
-      await getListEpisodes(request, res);
-    }
-
-    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
-      await getFilterEpisodes(request, res);
-    }
-  }
-);
+const server = http.createServer();
 
 const port = process.env.PORT;
 
